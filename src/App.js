175b90d1373079dcdb2b86306db36bd3852fc9e8
Faryal Naeem <faryalnaeem66@yahoo.com>
2020-06-30 12:24:52 +0500

    useEffect with data fetch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  let data = {title: "waiting for fetch"};
+  const [todo,setTodo] = useState(data);
+  const [isData,setData] = useState(false);
+  useEffect(()=>{
+    async function fetchData(){
+      const response =await  fetch('https://jsonplaceholder.typicode.com/todos/1');
+      console.log("response=",response);
+      
+      let data =await response.json();
+      setTodo(data) 
+    }
+   fetchData();
+  },[isData]);
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      hello fetch
+      <span> Title:{todo.title}</span>
     </div>
   );
 }
